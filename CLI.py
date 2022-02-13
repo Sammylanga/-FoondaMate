cef2b21b0b27acc08887ddda02d5866e02d4d708
Sammylanga <langasammy21@gmail.com>
2022-02-13 15:11:12 +0200

    updated the CLI app

--- CLI.py
+++ CLI.py
@@ -7,15 +7,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #fetch data from the API and returns it
-def fetchData(start,end):
+def fetchData():
+    #variable for checking formart
     format= "%d-%m-%Y"
-    try:
-        datetime.strptime(start, format)
-        datetime.strptime(end, format)
+    #loop until correct format is entered
+    var1=''
+    var2=''
+    while(1):
+        #allow user to enter args
+        start= input("Enter a start date to filter from: ")
+        end=input("Enter a end date to filter to: ")
+        try:
+            #check if is correct format the breaks if not
+            datetime.strptime(start, format)
+            datetime.strptime(end, format)
+            var1=start
+            var2=end
+            break
+        except:
+            print("This is the incorrect date string format. It should be dd-mm-yyyy")
         
-    except ValueError:
-        print("This is the incorrect date string format. It should be dd-mm-yyyy")
-        sys.exit(1)
     try:
         #connect to the API
         response=requests.get('http://sam-user-activity.eu-west-1.elasticbeanstalk.com/')
@@ -25,7 +36,7 @@ def fetchData(start,end):
         json_data = json.loads(data)
         #loop through the data and check to filter the dates
         for key in list(json_data):
-            if(key<start or key>end):
+            if(key<var1 or key>var2):
                 del json_data[key]
         #return the filtered data
         return json_data
@@ -38,17 +49,18 @@ def drawGraph(data):
     #changes data to list
     dates = list(data.keys())
     users = list(data.values())
-    fig = plt.figure(figsize= (10,7))
-    #set color to blue and width to 0.7 
-    plt.bar(dates, users ,color ='blue', width=0.7)
-    #label and plot the graph
-    plt.xlabel("Dates")
-    plt.ylabel("Number of active users")
-    plt.title("BarGraph for active user per given dates")
-    plt.show()
+    if(len(dates)>0):
+        fig = plt.figure(figsize= (10,7))
+        #set color to blue and width to 0.7 
+        plt.bar(dates, users ,color ='blue', width=0.7)
+        #label and plot the graph
+        plt.xlabel("Dates")
+        plt.ylabel("Number of active users")
+        plt.title("BarGraph for active user per given dates")
+        plt.show()
+    else:
+        print("No data to shows")
 
 if __name__ == "__main__":
     print("Dates should be in formart dd-mm-yyyy")
-    var1= input("Enter a start date to filter from: ")
-    var2=input("Enter a end date to filter to: ")
-    drawGraph(fetchData(var1,var2))
+    drawGraph(fetchData())
